567e226f6af7c110f725e38c4695eecf7eae12f3
SudhirSir <thesudhiir@gmail.com>
2026-06-12 17:56:00 +0530

    Configure pdf-parse as external package in next.config.mjs to resolve webpack build warning/error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverComponentsExternalPackages: ["pdf-parse"],
   images: {
     remotePatterns: [
       {
